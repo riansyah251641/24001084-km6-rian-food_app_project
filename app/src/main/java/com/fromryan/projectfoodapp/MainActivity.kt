dcfa4667c04f85f25fc43851041a3de1902eda3e
riansyah251641 <ggrhimo1641@gmail.com>
2024-03-22 21:14:19 +0700

    initial Project

---
+++ app/src/main/java/com/fromryan/projectfoodapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.fromryan.projectfoodapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fromryan.projectfoodapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
