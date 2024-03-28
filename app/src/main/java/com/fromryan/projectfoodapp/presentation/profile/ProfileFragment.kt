da71a5ac66eef8e4d8b4e60285982a2dd979bb05
riansyah251641 <ggrhimo1641@gmail.com>
2024-03-28 22:26:34 +0700

    add setting edit in profile

--- app/src/main/java/com/fromryan/projectfoodapp/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/fromryan/projectfoodapp/presentation/profile/ProfileFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.fromryan.projectfoodapp.R
 import com.fromryan.projectfoodapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private lateinit var binding : FragmentProfileBinding
+    private val viewModel: ProfileViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,6 +22,41 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
         return binding.root
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        getProfileData()
+        setClickListener()
+        changeEditMode()
+    }
+
+
+    private fun getProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.ivProfile.load(it.profileImg) {
+                crossfade(true)
+                error(R.drawable.ic_launcher_background)
+                transformations(CircleCropTransformation())
+            }
+            binding.etNameTextProfile.setText(it.name)
+            binding.etNomorTextProfile.setText(it.nohp)
+            binding.etEmailTextProfile.setText(it.email)
+        }
+    }
+
+    private fun setClickListener() {
+        binding.btnEditProfile.setOnClickListener {
+            viewModel.changeEditMode()
+        }
+    }
+    private fun changeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.etNameTextProfile.isEnabled = it
+            binding.etEmailTextProfile.isEnabled = it
+            binding.etNomorTextProfile.isEnabled = it
+        }
     }
 
 }
