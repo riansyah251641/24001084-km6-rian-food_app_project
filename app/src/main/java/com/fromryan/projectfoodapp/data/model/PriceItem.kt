781000d36e6b26f523d4fa482b1daad0ef1d1c13
riansyah251641 <ggrhimo1641@gmail.com>
2024-03-28 22:31:38 +0700

    save taks proggress checkout activity

---
+++ app/src/main/java/com/fromryan/projectfoodapp/data/model/PriceItem.kt
@@ -0,0 +1,6 @@
+package com.fromryan.projectfoodapp.data.model
+
+data class PriceItem(
+    val name: String,
+    val total: Double
+)
--- app/src/main/java/com/fromryan/projectfoodapp/presentation/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/fromryan/projectfoodapp/presentation/checkout/CheckoutViewModel.kt
@@ -1,6 +1,10 @@
 package com.fromryan.projectfoodapp.presentation.checkout
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.fromryan.projectfoodapp.data.repository.CartRepository
+import kotlinx.coroutines.Dispatchers
 
-class CheckoutViewModel:ViewModel() {
+class CheckoutViewModel(private val cartRepository: CartRepository) : ViewModel() {
+    val cartList = cartRepository.getUserCartData().asLiveData(Dispatchers.IO)
 }
