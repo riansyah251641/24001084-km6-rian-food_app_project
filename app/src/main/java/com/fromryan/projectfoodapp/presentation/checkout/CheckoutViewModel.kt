ee26ac4317ff74898775f35ea0e12fb7c7566d61
riansyah251641 <ggrhimo1641@gmail.com>
2024-03-29 20:31:13 +0700

    add desain 2

--- app/src/main/java/com/fromryan/projectfoodapp/presentation/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/fromryan/projectfoodapp/presentation/checkout/CheckoutViewModel.kt
@@ -2,10 +2,15 @@ package com.fromryan.projectfoodapp.presentation.checkout
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.fromryan.projectfoodapp.data.repository.CartRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class CheckoutViewModel(private val cartRepository: CartRepository) : ViewModel() {
     val checkoutData = cartRepository.getCheckoutData().asLiveData(Dispatchers.IO)
-    fun deleteAllCarts() = cartRepository.deleteAll()
+    fun deleteAllCarts(){
+        viewModelScope.launch(Dispatchers.IO){ cartRepository.deleteAll().collect() }
+    }
 }
